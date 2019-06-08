ebde3107f523220afd95f15789951791ea4991e4
a289237642 <a289237642@163.com>
2019-06-08 14:13:54 +0800

    all

--- react-admin/src/pages/login/login.jsx
+++ react-admin/src/pages/login/login.jsx
@@ -1,12 +1,13 @@
 import React, {Component} from 'react'
 import {
-  Form,
-  Icon,
-  Input,
-  Button
+    Form,
+    Icon,
+    Input,
+    Button
 } from 'antd'
 import './login.less'
 import logo from './images/logo.png'
+
 const Item = Form.Item // 不能写在import之前
 
 
@@ -15,119 +16,125 @@ const Item = Form.Item // 不能写在import之前
  */
 class Login extends Component {
 
-  handleSubmit = (event) => {
+    handleSubmit = (event) => {
 
-    // 阻止事件的默认行为
-    event.preventDefault()
+        // 阻止事件的默认行为
+        event.preventDefault()
+        //对所有的表单字段都要校验
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form:', values);
+            }else {
+                console.log("fail")
+            }
+        });
 
-
-
-    // 得到form对象
-    const form = this.props.form
-    // 获取表单项的输入数据
-    const values = form.getFieldsValue()
-    console.log('handleSubmit()', values)
-  }
-
-  /*
-  对密码进行自定义验证
-  */
-  /*
-   用户名/密码的的合法性要求
-     1). 必须输入
-     2). 必须大于等于4位
-     3). 必须小于等于12位
-     4). 必须是英文、数字或下划线组成
-    */
-  validatePwd = (rule, value, callback) => {
-    console.log('validatePwd()', rule, value)
-    if(!value) {
-      callback('密码必须输入')
-    } else if (value.length<4) {
-      callback('密码长度不能小于4位')
-    } else if (value.length>12) {
-      callback('密码长度不能大于12位')
-    } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
-      callback('密码必须是英文、数字或下划线组成')
-    } else {
-      callback() // 验证通过
+        // 得到form对象
+        // const form = this.props.form
+        // // 获取表单项的输入数据
+        // const values = form.getFieldsValue()
+        // console.log('handleSubmit()', values)
     }
-    // callback('xxxx') // 验证失败, 并指定提示的文本
-  }
 
-  render () {
+    /*
+    对密码进行自定义验证
+    */
+    /*
+     用户名/密码的的合法性要求
+       1). 必须输入
+       2). 必须大于等于4位
+       3). 必须小于等于12位
+       4). 必须是英文、数字或下划线组成
+      */
+    validatePwd = (rule, value, callback) => {
+        console.log('validatePwd()', rule, value)
+        if (!value) {
+            callback('密码必须输入')
+        } else if (value.length < 4) {
+            callback('密码长度不能小于4位')
+        } else if (value.length > 12) {
+            callback('密码长度不能大于12位')
+        } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+            callback('密码必须是英文、数字或下划线组成')
+        } else {
+            callback() // 验证通过
+        }
+        // callback('xxxx') // 验证失败, 并指定提示的文本
+    }
 
-    // 如果用户已经登陆, 自动跳转到管理界面
+    render() {
 
-    // 得到具强大功能的form对象
-    const form = this.props.form
-    const { getFieldDecorator } = form;
+        // 如果用户已经登陆, 自动跳转到管理界面
 
-    return (
-      <div className="login">
-        <header className="login-header">
-          <img src={logo} alt="logo"/>
-          <h1>React项目: 后台管理系统</h1>
-        </header>
-        <section className="login-content">
-          <h2>用户登陆</h2>
-          <Form onSubmit={this.handleSubmit} className="login-form">
-            <Item>
-              {
-                /*
-              用户名/密码的的合法性要求
-                1). 必须输入
-                2). 必须大于等于4位
-                3). 必须小于等于12位
-                4). 必须是英文、数字或下划线组成
-               */
-              }
-              {
-                getFieldDecorator('username', { // 配置对象: 属性名是特定的一些名称
-                  // 声明式验证: 直接使用别人定义好的验证规则进行验证
-                  rules: [
-                    { required: true, whitespace: true, message: '用户名必须输入' },
-                    { min: 4, message: '用户名至少4位' },
-                    { max: 12, message: '用户名最多12位' },
-                    { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成' },
-                  ],
-                  initialValue: 'admin', // 初始值
-                })(
-                  <Input
-                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    placeholder="用户名"
-                  />
-                )
-              }
-            </Item>
-            <Form.Item>
-              {
-                getFieldDecorator('password', {
-                  rules: [
-                    {
-                      validator: this.validatePwd
-                    }
-                  ]
-                })(
-                  <Input
-                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    type="password"
-                    placeholder="密码"
-                  />
-                )
-              }
+        // 得到具强大功能的form对象
+        const form = this.props.form
+        const {getFieldDecorator} = form;
 
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit" className="login-form-button">
-                登陆
-              </Button>
-            </Form.Item>
-          </Form>
-        </section>
-      </div>
-    )
-  }
+        return (
+            <div className="login">
+                <header className="login-header">
+                    <img src={logo} alt="logo"/>
+                    <h1>React项目: 后台管理系统</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登陆</h2>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Item>
+                            {
+                                /*
+                              用户名/密码的的合法性要求
+                                1). 必须输入
+                                2). 必须大于等于4位
+                                3). 必须小于等于12位
+                                4). 必须是英文、数字或下划线组成
+                               */
+                            }
+                            {
+                                getFieldDecorator('username', { // 配置对象: 属性名是特定的一些名称
+                                    // 声明式验证: 直接使用别人定义好的验证规则进行验证
+                                    rules: [
+                                        {required: true, whitespace: true, message: '用户名必须输入'},
+                                        {min: 4, message: '用户名至少4位'},
+                                        {max: 12, message: '用户名最多12位'},
+                                        {pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成'},
+                                    ],
+                                    initialValue: 'admin', // 初始值
+                                })(
+                                    <Input
+                                        prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                        placeholder="用户名"
+                                    />
+                                )
+                            }
+                        </Item>
+                        <Form.Item>
+                            {
+                                getFieldDecorator('password', {
+                                    rules: [
+                                        {
+                                            validator: this.validatePwd
+                                        }
+                                    ]
+                                })(
+                                    <Input
+                                        prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                        type="password"
+                                        placeholder="密码"
+                                    />
+                                )
+                            }
+
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                登陆
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
+            </div>
+        )
+    }
 }
 
 /*
