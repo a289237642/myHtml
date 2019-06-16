c4eac5e467bd0e98720a85223cc981a6e6af777a
a289237642 <a289237642@163.com>
2019-06-16 15:05:52 +0800

    商品分类模块

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,19 +1,19 @@
 import React, {Component} from 'react'
-import {BrowserRouter, Route,Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Login from './pages/login/login'
 import Admin from './pages/admin/admin'
 
-
-/**应用的根组件**/
-
+/*
+应用的根组件
+ */
 export default class App extends Component {
 
 
     render() {
         return (
             <BrowserRouter>
-                <Switch>
+                <Switch> {/*只匹配其中一个*/}
                     <Route path='/login' component={Login}></Route>
                     <Route path='/' component={Admin}></Route>
                 </Switch>
@@ -21,4 +21,3 @@ export default class App extends Component {
         )
     }
 }
-
