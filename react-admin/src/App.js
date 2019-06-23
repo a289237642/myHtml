b202926e12fcc2c46ad54a791877145b02cd7235
a289237642 <a289237642@163.com>
2019-06-23 21:55:02 +0800

    all

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,23 +1,74 @@
 import React, {Component} from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-
-import Login from './pages/login/login'
-import Admin from './pages/admin/admin'
 
 /*
 应用的根组件
  */
 export default class App extends Component {
 
+    state = {
+        count: 0
+    }
+
+    constructor(props) {
+        super(props)
+
+
+        this.numberRef = React.createRef()
+    }
+
+    increment = () => {
+        const number = this.numberRef.current.value * 1
+        this.setState(state => ({
+            count: state.count + number
+        }))
+    }
+    decrement = () => {
+        const number = this.numberRef.current.value * 1
+        this.setState(state => ({
+            count: state.count - number
+        }))
+    }
+
+    incrementIfOdd = () => {
+        const number = this.numberRef.current.value * 1
+        if (this.state.count % 2 === 1) {
+            this.setState(state => ({
+                count: state.count + number
+            }))
+        }
+
+    }
+
+    incrementAsync = () => {
+        const number = this.numberRef.current.value * 1
+
+        setTimeout(() => {
+            this.setState(state => ({
+                count: state.count + number
+            }))
+        }, 1000)
+
+    }
+
 
     render() {
+        const count = this.state.count
         return (
-            <BrowserRouter>
-                <Switch> {/*只匹配其中一个*/}
-                    <Route path='/login' component={Login}></Route>
-                    <Route path='/' component={Admin}></Route>
-                </Switch>
-            </BrowserRouter>
+            <div>
+                <p>click {count} times</p>
+                <div>
+                    <section ref={this.numberRef}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </section>
+                    <button onClick={this.increment}>+</button>
+                    <button onClick={this.decrement}>-</button>
+                    <button onClick={this.incrementIfOdd}>increment if odd</button>
+                    <button onClick={this.incrementAsync}>increment</button>
+
+                </div>
+            </div>
         )
     }
 }
