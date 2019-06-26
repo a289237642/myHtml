7a1c6174307386a53047b0653bdc1fcb1fdf198c
a289237642 <a289237642@163.com>
2019-06-26 23:51:07 +0800

    ar

--- Test/Test/spiders/baidu.py
+++ Test/Test/spiders/baidu.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-import scrapy
+import scrapy,time
 from selenium import webdriver
 from lxml import etree
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
 
 class BaiduSpider(scrapy.Spider):
@@ -18,39 +19,84 @@ class BaiduSpider(scrapy.Spider):
         chrome_options.add_argument('--disable-dev-shm-usage')
         driver = webdriver.Chrome()
         driver.get(response.url)
-        htmls = etree.HTML(driver.page_source)
-
         dicts = {}
-        if len(htmls.xpath('//h2[@id="activity-name"]/text()')) > 0:
-            dicts['tit'] = htmls.xpath(
-                '//h2[@id="activity-name"]/text()')[0].strip()
+        # htmls = etree.HTML(driver.page_source)
+        selector = etree.HTML(driver.page_source)
+        title = selector.xpath('//*[@id="activity-name"]/text()')[0]
+        title = "".join(title.split())
+        name = selector.xpath('//*[@id="js_name"]/text()')[0]
+        name = "".join(name.split())
+        # date = selector.xpath('//*[@id="publish_time"]/text()')[0]
+        spider_time = time.time()
+        date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(spider_time))
+        dicts['title'] = title
+        dicts['name'] = name
+        dicts['date'] = date
+        dicts['web_name'] = name
+
+        is_original = selector.xpath('//*[@id="copyright_logo"]/text()')
+        if len(is_original) == 0:
+            dicts['is_original'] = 0  # 不是原创
         else:
-            dicts['tit'] = ""
-        if len(htmls.xpath('//*[@id="publish_time"]/text()')) > 0:
-            dicts['mtime'] = htmls.xpath('//*[@id="publish_time"]/text()')[0]
-        else:
-            dicts['mtime'] = ""
-        if len(htmls.xpath('//span[@id="js_author_name"]/text()')) > 0:
-            dicts['author'] = htmls.xpath(
-                '//span[@id="js_author_name"]/text()')[0]
-        else:
-            dicts['author'] = ""
-        # content=''.join(htmls.xpath('//div[@id="js_content"]/p/span/text()')
-        if len(driver.find_element_by_xpath(
-                '//div[@id="js_content"]').get_attribute('outerHTML')) > 0:
-            dicts['content'] = driver.find_element_by_xpath(
-                '//div[@id="js_content"]').get_attribute('outerHTML')
-        else:
-            dicts['content'] = ""
-        if len(htmls.xpath('//*[@id="js_content"]/p[5]/img/@data-src')) > 0:
-            dicts['img_url'] = htmls.xpath(
-                '//*[@id="js_content"]/p[5]/img/@data-src')[0]
-        else:
-            dicts['img_url'] = ""
+            dicts['is_original'] = 1  # 是原创
+
+        # 用xpath取出需要的内容页面部分
+        result = selector.xpath('//*[@id="js_content"]')[0]
+        # 转为html
+        content = etree.tostring(result, method='html')
+        # 获取bs4对象
+        soup = BeautifulSoup(content, 'html.parser', from_encoding='utf-8')
+        new_list = []
+        ls = soup.find_all(["p", "img"])
+        for table in ls:
+            res = {}
+
+            data = table.get_text()
+            if data:
+                # 去除空字符和特殊字符
+                new_data = "".join(data.split())
+                new_data = new_data.replace('\ufeff', '')
+                if new_data != "":
+                    res["text"] = new_data
+                    new_list.append(res)
+
+            link = table.get('data-src')
+            print(link)
+            res["img"] = link
+        dicts['content'] = new_list
+
+        print(dicts)
+        # dicts = {}
+        # if len(htmls.xpath('//h2[@id="activity-name"]/text()')) > 0:
+        #     dicts['tit'] = htmls.xpath(
+        #         '//h2[@id="activity-name"]/text()')[0].strip()
+        # else:
+        #     dicts['tit'] = ""
+        # if len(htmls.xpath('//*[@id="publish_time"]/text()')) > 0:
+        #     dicts['mtime'] = htmls.xpath('//*[@id="publish_time"]/text()')[0]
+        # else:
+        #     dicts['mtime'] = ""
+        # if len(htmls.xpath('//span[@id="js_author_name"]/text()')) > 0:
+        #     dicts['author'] = htmls.xpath(
+        #         '//span[@id="js_author_name"]/text()')[0]
+        # else:
+        #     dicts['author'] = ""
+        # # content=''.join(htmls.xpath('//div[@id="js_content"]/p/span/text()')
+        # if len(driver.find_element_by_xpath(
+        #         '//div[@id="js_content"]').get_attribute('outerHTML')) > 0:
+        #     dicts['content'] = driver.find_element_by_xpath(
+        #         '//div[@id="js_content"]').get_attribute('outerHTML')
+        # else:
+        #     dicts['content'] = ""
+        # if len(htmls.xpath('//*[@id="js_content"]/p[5]/img/@data-src')) > 0:
+        #     dicts['img_url'] = htmls.xpath(
+        #         '//*[@id="js_content"]/p[5]/img/@data-src')[0]
+        # else:
+        #     dicts['img_url'] = ""
 
         # print("=====tit=======>>", tit)
         # print("======mtime=======>>", mtime)
         # print("======author=======>>", author)
         # print("======content=======>>", content)
         # print("======img_url=======>>", img_url)
-        print(dicts)
+        # print(dicts)
